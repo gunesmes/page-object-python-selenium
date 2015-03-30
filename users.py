f1eeeb3fdf9bc8060c379121825dbae5548054af
Mesut Güneş <gunesmes@gmail.com>
2015-03-30 10:52:37 +0300

    updated get_user

--- users.py
+++ users.py
@@ -13,6 +13,6 @@ users = [
 
 def get_user(name):
 	try:
-		return (user for user in users if user["name" == name]).next()
+		return (user for user in users if user["name"] == name).next()
 	except:
 		print "\n     User %s is not defined, enter a valid user.\n" %name
