24a4dbbbc207c4352257fed35b82057b352d796b
Mesut Güneş <gunesmes@gmail.com>
2015-03-30 10:34:38 +0300

    updated user function

--- users.py
+++ users.py
@@ -12,8 +12,7 @@ users = [
 ]
 
 def get_user(name):
-	users_ = sorted(users, key=itemgetter("name"))
-	for user in users_:
-		if user["name"] == name:
-			break
-	return user
+	try:
+		return (user for user in users if user["name" == name]).next()
+	except:
+		print "\n     User %s is not defined, enter a valid user.\n" %name
