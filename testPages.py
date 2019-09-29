68c3bb00c953aa4fec2e2397ab75897c6551d4fa
Ravichandran Ganapathy <psg.ravi@gmail.com>
2019-09-29 22:11:37 +1000

    fixed print statement

--- testPages.py
+++ testPages.py
@@ -17,37 +17,37 @@ class TestPages(unittest.TestCase):
         self.driver.get("http://www.amazon.com")
 
     def test_page_load(self):
-        print "\n" + str(test_cases(0))
+        print ("\n" + str(test_cases(0)))
         page = MainPage(self.driver)
         self.assertTrue(page.check_page_loaded())
 
     def test_search_item(self):
-        print "\n" + str(test_cases(1))
+        print ("\n" + str(test_cases(1)))
         page = MainPage(self.driver)
         search_result = page.search_item("Nexus 5")
         self.assertIn("Nexus 5", search_result)
 
     def test_sign_up_button(self):
-        print "\n" + str(test_cases(2))
+        print ("\n" + str(test_cases(2)))
         page = MainPage(self.driver)
         signUpPage = page.click_sign_up_button()
         self.assertIn("ap/register", signUpPage.get_url())
 
     def test_sign_in_button(self):
-        print "\n" + str(test_cases(3))
+        print ("\n" + str(test_cases(3)))
         page = MainPage(self.driver)
         loginPage = page.click_sign_in_button()
         self.assertIn("ap/signin", loginPage.get_url())
 
     def test_sign_in_with_valid_user(self):
-        print "\n" + str(test_cases(4))
+        print ("\n" + str(test_cases(4)))
         mainPage = MainPage(self.driver)
         loginPage = mainPage.click_sign_in_button()
         result = loginPage.login_with_valid_user("valid_user")
         self.assertIn("yourstore/home", result.get_url())
 
     def test_sign_in_with_in_valid_user(self):
-        print "\n" + str(test_cases(5))
+        print ("\n" + str(test_cases(5)))
         mainPage = MainPage(self.driver)
         loginPage = mainPage.click_sign_in_button()
         result = loginPage.login_with_in_valid_user("invalid_user")
